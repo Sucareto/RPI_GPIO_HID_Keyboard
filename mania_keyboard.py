44830ceaa955ef3851b3faa6fe68f1ccafdade62
Sucaretto <28331534+Sucaretto@users.noreply.github.com>
2019-01-07 17:01:16 +0800

    添加两个按键，修改判断逻辑，减少调用。

--- mania_keyboard.py
+++ mania_keyboard.py
@@ -3,15 +3,22 @@ import RPi.GPIO as GPIO
 import signal
 import time
 
-P_1 = 37
-P_2 = 40
-P_3 = 38
-P_4 = 33
+P_1 = 33
+P_2 = 35
+P_3 = 37
+
+P_4 = 36
+P_5 = 38
+P_6 = 40
 
 K_1 = "\x07"	#D
 K_2 = "\x09"	#F
-K_3 = "\x0d"	#J
-K_4 = "\x0e"	#K
+
+K_3 = "\x0a"    #G
+K_4 = "\x0b"    #H
+
+K_5 = "\x0d"	#J
+K_6 = "\x0e"	#K
 
 keycode_tmp = ""
 
@@ -29,9 +36,11 @@ GPIO.setup(P_1,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(P_2,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(P_3,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(P_4,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(P_5,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(P_6,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 while 1:
-	k1,k2,k3,k4 = "","","",""
+	k1,k2,k3,k4,k5,k6 = "","","","","",""
 
         if GPIO.input(P_1) is GPIO.LOW:
 		k1 = K_1
@@ -45,11 +54,17 @@ while 1:
 	if GPIO.input(P_4) is GPIO.LOW:
                 k4 = K_4
 
-	ks = "\x00" * 2 + k1 + k2 + k3 + k4
-	keycode = ks + "\x00" * ( 8 - len(ks))
+        if GPIO.input(P_5) is GPIO.LOW:
+                k5 = K_5
 
-	if keycode_tmp != keycode:
-		keycode_tmp = keycode
+        if GPIO.input(P_6) is GPIO.LOW:
+                k6 = K_6
+
+	ks = "\x00" * 2 + k1 + k2 + k3 + k4 + k5 + k6
+
+	if keycode_tmp != ks:
+		keycode_tmp = ks
+		keycode = ks + "\x00" * ( 8 - len(ks))
 		f = open("/dev/hidg0","w");
 	        f.write(keycode[0:8])
 	        f.close()
