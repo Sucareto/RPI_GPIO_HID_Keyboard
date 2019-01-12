4a7a2d802e6aeb1d7bab25af88877979bf8a5b02
Sucaretto <28331534+Sucaretto@users.noreply.github.com>
2019-01-13 02:47:54 +0800

    模拟 sdvx 手台的脚本编写完成！

---
+++ sdvx_controller.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import signal
+import time
+
+KEY_LIST = {
+	'V-L1':{'PIN':37,'KEY':'\x14'},#Q
+	'V-L2':{'PIN':35,'KEY':'\x1a'},#W
+	'V-R1':{'PIN':15,'KEY':'\x12'},#O
+	'V-R2':{'PIN':13,'KEY':'\x13'},#P
+
+	'BT-A':{'PIN':33,'KEY':'\x07'},#D
+	'BT-B':{'PIN':31,'KEY':'\x09'},#F
+	'BT-C':{'PIN':11,'KEY':'\x0d'},#J
+	'BT-D':{'PIN':18,'KEY':'\x0e'},#K
+
+	'FX-L':{'PIN':29,'KEY':'\x0a'},#G
+	'FX-R':{'PIN':16,'KEY':'\x0b'},#H
+}
+
+keycode_tmp = "\x00" * 8
+
+def signal_handler(signal,frame):
+	f = open("/dev/hidg0","w");
+	f.write("\x00" * 8)
+	f.close()
+	GPIO.cleanup()
+	exit()
+
+signal.signal(signal.SIGINT,signal_handler)
+
+GPIO.setmode(GPIO.BOARD)
+for P in KEY_LIST.keys():
+	GPIO.setup(KEY_LIST[P]['PIN'],GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+
+while 1:
+	ks = "\x00" * 2
+	for K in KEY_LIST.keys():
+		if GPIO.input(KEY_LIST[K]['PIN']) is GPIO.LOW:
+			ks += KEY_LIST[K]['KEY']
+
+	if keycode_tmp != ks:
+		keycode_tmp = ks
+		keycode = ks + "\x00" * ( 8 - len(ks))
+		f = open("/dev/hidg0","w");
+	        f.write(keycode[0:8])
+	        f.close()
+
+	time.sleep(0.01)
