0cf52ef858de020713639f0c3db3c257a56c365a
Sucaretto <28331534+Sucaretto@users.noreply.github.com>
2019-01-23 00:24:34 +0800

    修正一处致命的错误。

--- Setup/USB_Setup.sh
+++ Setup/USB_Setup.sh
@@ -4,8 +4,8 @@ if [[ $UID -eq 0 ]];then
 	if [ ! -f /etc/modules ]; then touch /etc/modules; fi
 	sed -n -i -e '/^dwc2/!p' -e '$adwc2' /etc/modules
 	sed -n -i -e '/^libcomposite/!p' -e '$alibcomposite' /etc/modules
-	cp ./HID_config.sh /opt/
-	sed -i 's/exit 0/\/opt\/HID_config.sh\nexit 0/g' /etc/rc.local
+	cp ./HID_config /opt/ && chmod 700 /opt/HID_config
+	sed -i 's/^exit 0$/\/opt\/HID_config\n&/g' /etc/rc.local
 	echo "执行完成，如无错误提示即为成功，请重启后使用"
 else
 	echo "请使用 root 执行。"
