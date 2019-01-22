26a0a1756512bb698d1715be1326f6548c478804
Sucaretto <28331534+Sucaretto@users.noreply.github.com>
2019-01-23 00:47:43 +0800

    修复 /dev/hidg0 没有权限写入的错误。

--- Setup/USB_Setup.sh
+++ Setup/USB_Setup.sh
@@ -4,7 +4,7 @@ if [[ $UID -eq 0 ]];then
 	if [ ! -f /etc/modules ]; then touch /etc/modules; fi
 	sed -n -i -e '/^dwc2/!p' -e '$adwc2' /etc/modules
 	sed -n -i -e '/^libcomposite/!p' -e '$alibcomposite' /etc/modules
-	cp ./HID_config /opt/ && chmod 700 /opt/HID_config
+	cp ./HID_config /opt/ && chmod 744 /opt/HID_config
 	sed -i 's/^exit 0$/\/opt\/HID_config\n&/g' /etc/rc.local
 	echo "执行完成，如无错误提示即为成功，请重启后使用"
 else
