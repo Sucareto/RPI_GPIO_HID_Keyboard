3e795a029639b2acd1be6ecd95ba64703dd67625
Sucaretto <28331534+Sucaretto@users.noreply.github.com>
2019-02-02 13:35:24 +0800

    修改初始化方式，防止因为初始化太慢被提示 “工作不正常” 。

--- Setup/USB_Setup.sh
+++ Setup/USB_Setup.sh
@@ -5,7 +5,15 @@ if [[ $UID -eq 0 ]];then
 	sed -n -i -e '/^dwc2/!p' -e '$adwc2' /etc/modules
 	sed -n -i -e '/^libcomposite/!p' -e '$alibcomposite' /etc/modules
 	cp ./HID_config /opt/ && chmod 744 /opt/HID_config
-	sed -i 's/^exit 0$/\/opt\/HID_config\n&/g' /etc/rc.local
+	cat << EOF | tee /etc/systemd/system/hid-setup.service > /dev/null
+[Unit]
+Description=hid setup
+[Service]
+ExecStart=/opt/HID_config
+[Install]
+WantedBy=multi-user.target
+EOF
+	systemctl enable hid-setup.service
 	echo "执行完成，如无错误提示即为成功，请重启后使用"
 else
 	echo "请使用 root 执行。"
